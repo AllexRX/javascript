281fdca8b378ae6c354a30f8762a24e48cd65a3f
Alex <acs8095@gmail.com>
2023-02-24 21:24:25 -0300

    create ex010.html

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let velocidade = (45.7)
+console.log(`A velocidade atual do seu carro é de ${velocidade}Km/h`)
+if (velocidade > 60) {
+   console.log(`Você está acima do limite de velocidade, você foi MULTADO!!`)
+}
+console.log(`Dirija sempre com cuidado!!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+let country = 'bro'
+console.log(`You live in ${country}`)
+if (country == 'Brasil') {
+   console.log(`You are Brasileiro`)
+} else {
+   console.log(`You are foreigner`)
+}
