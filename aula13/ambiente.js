3a85ba9c9e95e17396ca06f8abc7b18f1730c072
Alex <acs8095@gmail.com>
2023-03-01 21:12:55 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+   console.log(`passo ${c}`)
+   c++ 
+} while (c <= 10);
+
+// A ESTRUTURA DE CIMA E A DE BAIXO SÂO ESSÊNCIALMENTE A MESMA COISA
+// A DE BAIXO É A MAIS UTILIZADA
+
+var c = 1
+while (c <= 20) {
+   console.log(`passo ${c}`)
+   c++
+}
+
+// c++ é o mesmo que c = c + 1
+/*
+console.log('tudo bem?');
+console.log('tudo bem?');
+console.log('tudo bem?');
+console.log('tudo bem?');
+*/
