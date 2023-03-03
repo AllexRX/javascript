d32cd815dd1601f371531e30a59ca39d62b86074
Alex <acs8095@gmail.com>
2023-03-03 18:37:10 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+console.log('Vai começar...')
+for (c = 1; c <= 5; c++) {
+   console.log(c);
+}
+console.log('FIM')
+
+/*
+essa estrutura a cima tem a mesma função que a de cima
+var c = 1
+while (c <= 5) {
+   console.log(`${c}`)
+   c++
+}
+*/
