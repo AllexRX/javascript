0efb8ea7e13a61b604ac0537d41313d006fa8be4
Alex <acs8095@gmail.com>
2023-03-04 01:58:31 -0300

    create desafo2

---
+++ desafios/desafio2_boas_vindas/script.js
@@ -0,0 +1,5 @@
+function iniciar() {
+   let name = prompt('Qual é o seu nome?');
+   let age = prompt('Quantos anos você tem?');
+   alert(`Acabei de conhecer o(a) ${name} que tem ${age} anos. Muito prazer em te conhecer!`)
+}
