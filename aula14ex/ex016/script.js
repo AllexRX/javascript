2bc97a915444e9caada02d3d9398cca119bdd2ff
Alex <acs8095@gmail.com>
2023-03-03 23:46:04 -0300

    .

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,13 +1,29 @@
 function criar() {
-   var inicio = 10
-   var fim = 2
-   var passo = document.querySelector('#id_passo').value;
-   if (inicio > fim) {
-      for(var menos = inicio; menos >= fim; menos = menos - 1)
-      console.log = (menos)
+   let inicio = document.getElementById('id_inicio');
+   let fim = document.getElementById('id_fim');
+   let passo = document.getElementById('id_passo');
+   let resultado = document.getElementById('res');
+   if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0) {
+      resultado.innerHTML = '[ERROR] Informe todos os campos'
+   } else {
+      resultado.innerHTML = 'Contando: <br>'
+      let ini = Number(inicio.value)
+      let f = Number(fim.value)
+      let p = Number(passo.value)
+      if (p <= 0) {
+         alert('Passo invalido, considerando passo 1')
+         p = 1
+      }
+      if (ini < f) {
+         // contagem crescente
+         for(let c = ini; c <= f; c += p) {
+            resultado.innerHTML += `${c}, \u{1F449}`
+         }
+      } else {
+         // contagem decrescente
+         for(let c = ini; c >= f; c -= p)
+         resultado.innerHTML += `${c}, \u{1F449}`
+      }
+      resultado.innerHTML += `\u{1F3C1}`
    }
-}
-
-
-//document.querySelector('#id_inicio').value;
-//document.querySelector('#id_fim').value;
+}  
