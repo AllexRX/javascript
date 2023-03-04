261194fa48721db229b038d0300aa5229ab9b96c
Alex <acs8095@gmail.com>
2023-03-04 01:51:21 -0300

    create desafio1

---
+++ desafios/desafio_das_mensagens/script.js
@@ -0,0 +1,9 @@
+function botton1() {
+   window.alert('Você clicou no botão 1');
+}
+function botton2() {
+   alert('Você clicou no botão 2')
+}
+function botton3() {
+   alert('Você clicou no botão 3')
+}
