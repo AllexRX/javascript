d9e2ed01204dc8bfb909a1988f474c7991408c04
Alex <acs8095@gmail.com>
2023-03-08 21:46:57 -0300

    .

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -2,6 +2,7 @@ let num = document.querySelector('input#num');
 let lista = document.querySelector('select#numeros_valores');
 let res = document.querySelector('div#res')
 let valores = []; //aqui foi declarado um array vazio para ser usado mais para frente adicionando os numeros da lista
+
 function numero(n) { // função criada para verificar se os numeros estão corretos
    if (Number(n) >= 1 && Number(n) <= 100) {
       return true;
@@ -20,8 +21,40 @@ function inlista(n , l) {
 
 function adicionar() {
    if(numero(num.value) && !inlista(num.value, valores)) {
-      alert('Tudo ok');
+      valores.push(Number(num.value));
+      let item = document.createElement('option');
+      item.text = `Valor ${num.value} adicionado`;
+      lista.appendChild(item);
+      res.innerHTML = ``;
    } else {
       alert('Número invalido ou já existente na lista');
+   }
+   num.value = '';
+   num.focus();
+}
+
+function finalizar() {
+   if (valores.lenght == 0) {
+      alert('Nenhum valor adicionado, adicone valores antes de finalizar.');
+   } else {
+      tot = valores.length
+      let maior = valores[0];
+      let menor = valores[0];
+      let soma = 0;
+      let media = 0;
+      for(let pos in num) {
+         soma += valores[pos];
+         if (num[pos] > maior) {
+            maior = valores[pos];
+         } else if (num[pos] < menor) {
+         menor = valores[pos];
+         }
+      }
+      media = soma / tot
+      res.innerHTML = ``
+      res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`;
+      res.innerHTML += `<p>O maior valor foi ${maior} e o menor ${menor}</p>`;
+      res.innerHTML += `<p>A soma entre os valores digitados pelos usuario é equivalente a ${soma}</p>`;
+      res.innerHTML +=  `<p> A média dos valores é ${media}</p>`;
    }
 }
